e2d96f6c52d1117c297df6e1785ce18199b9febf
Manthan Dave <manthanhd@live.com>
2014-03-27 17:47:15 +0000

    Changes to web.js. Setting things up for bromleyWS.

--- web.js
+++ web.js
@@ -5,7 +5,34 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World! This is awesome speaking. Yes. Awesome.');
+  res.write("<html><body><p>");
+  res.write('Hello World! This is awesome speaking. Yes. Awesome.');
+  res.write("<br/>");
+  res.write("Use /mongo.ws/search and pass params like searchText, resultsCount and pageNumber in order to query posts from mongodb.");
+  res.write("</p></body></html>");
+  res.end();
+});
+
+app.get('/mongo.ws', function(req, res) {
+  res.send('Hello World! Welcome to mongo.ws!');
+});
+
+
+app.get('/mongo.ws/search', function(req, res) {
+  if(typeof req.query.searchText !== 'undefined' && typeof req.query.resultsCount !== 'undefined' && typeof req.query.pageNumber !== 'undefined'){
+	// Request is valid.
+	var searchText = req.query.searchText;
+	var resultsCount = req.query.resultsCount;
+	var pageNumber = req.query.pageNumber;
+	res.write("<html><body><p>");
+	res.write("searchText: " + searchText + "<br/>");
+	res.write("resultsCount: " + resultsCount + "<br/>");
+	res.write("pageNumber: " + pageNumber + "<br/>");
+	res.write("</p></body></html>");
+	res.end();
+  } else {
+  	res.send('Hello World! Welcome to mongo.ws!"' + req.query.searchText + '"');
+  }
 });
 
 var port = Number(process.env.PORT || 5000);
