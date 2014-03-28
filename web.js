ed21bd7dc30d1b1527d403fb66022f119c1b4d9d
Manthan Dave <manthanhd@live.com>
2014-03-28 18:36:47 +0000

    MongoDB Addon added.

--- web.js
+++ web.js
@@ -5,34 +5,24 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.write("<html><body><p>");
-  res.write('Hello World! This is awesome speaking. Yes. Awesome.');
-  res.write("<br/>");
-  res.write("Use /mongo.ws/search and pass params like searchText, resultsCount and pageNumber in order to query posts from mongodb.");
-  res.write("</p></body></html>");
-  res.end();
-});
-
-app.get('/mongo.ws', function(req, res) {
   res.send('Hello World! Welcome to mongo.ws!');
 });
 
 
-app.get('/mongo.ws/search', function(req, res) {
-  if(typeof req.query.searchText !== 'undefined' && typeof req.query.resultsCount !== 'undefined' && typeof req.query.pageNumber !== 'undefined'){
-	// Request is valid.
-	var searchText = req.query.searchText;
-	var resultsCount = req.query.resultsCount;
-	var pageNumber = req.query.pageNumber;
-	res.write("<html><body><p>");
-	res.write("searchText: " + searchText + "<br/>");
-	res.write("resultsCount: " + resultsCount + "<br/>");
-	res.write("pageNumber: " + pageNumber + "<br/>");
-	res.write("</p></body></html>");
-	res.end();
-  } else {
-  	res.send('Hello World! Welcome to mongo.ws!"' + req.query.searchText + '"');
-  }
+app.get('/search', function(req, res) {
+	var mongo = require("mongodb");
+	var mongoUri = process.env.MONGOLAB_URI ||
+		  process.env.MONGOHQ_URL ||
+		  'mongodb://localhost/mydb';
+	
+	mongo.Db.connect(mongoUri, function (err, db) {
+	  db.collection('twitch', function(er, collection) {
+	    collection.find().toArray(function (err, docs) {
+		res.send("Found " + docs.length + " twitches.");
+	    });
+	    
+	  });
+	});
 });
 
 var port = Number(process.env.PORT || 5000);
